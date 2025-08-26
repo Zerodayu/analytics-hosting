75c6b1122dd4357ed3c947b5d96503b25b84eb88
- Z e r o <69074961+Zerodayu@users.noreply.github.com>
2025-08-27 04:31:01 +0800

    Delete frontend directory

--- frontend/app/batch-list/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BatchListPage = () => {
-  return (
-    <div>Batch List</div>
-  )
-}
-
-export default BatchListPage
--- frontend/app/create-batch/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateBatchPage = () => {
-  return (
-    <div>Create Batch</div>
-  )
-}
-
-export default CreateBatchPage
